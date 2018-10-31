160b221867cced1bc3c6c46c3b7e5ddd45603a10
syukan3 <en421430@s.okayama-u.ac.jp>
2018-10-31 23:35:46 +0900

    10/31 up-to-date

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  belongs_to :user, optional: true
+  belongs_to :stock, optional: true
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,4 @@
 class Stock < ApplicationRecord
+  belongs_to :user, optional: true
+  has_many :orders, dependent: :destroy
 end
