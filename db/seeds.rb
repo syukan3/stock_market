13c8b5ba3f9e47be221868ee94d4faf265be489b
syukan3 <en421430@s.okayama-u.ac.jp>
2018-11-03 19:25:05 +0900

    Input information of issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "csv"
+
+CSV.foreach('db/data_j.csv') do |row|
+  Issue.create(:stock_number => row[0], :company_name => row[1])
+end
+
 5.times do |i|
   User.create(email: "x#{i+1}@x.com", available: (i+1) * 1000000, password: "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}")
   # Order.create(user_id: i, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
@@ -14,5 +20,4 @@
   # Order.create(user_id: i+3, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
   # Order.create(user_id: i+4, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
   Stock.create(current_price: i, max_price: i, min_price: i, start_price: i, end_price: i, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
-  Issue.create(stock_number: i, company_name: "#{i}_company")
 end
