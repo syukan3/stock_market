ae91a166b74fe2c9dde99fc6d688d4fb7a7799df
syukan3 <en421430@s.okayama-u.ac.jp>
2018-11-04 20:45:05 +0900

    Input stock data(1301)

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,17 @@ CSV.foreach('db/data_j.csv') do |row|
   Issue.create(:stock_number => row[0], :company_name => row[1])
 end
 
-5.times do |i|
+CSV.foreach('db/1301_2018.csv') do |row|
+  Stock.create(:max_price => row[2], :min_price => row[3], :start_price => row[1], :end_price => row[4], :date => row[0], :stock_number => row[7])
+end
+
+20.times do |i|
   User.create(email: "x#{i+1}@x.com", available: (i+1) * 1000000, password: "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}" + "#{i+1}")
   # Order.create(user_id: i, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
+  # Stock.create(current_price: i, max_price: i, min_price: i, start_price: i, end_price: i, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
+end
 
-  Stock.create(current_price: i, max_price: i, min_price: i, start_price: i, end_price: i, stock_number: "#{i}" + "#{i+1}" + "#{i+2}" + "#{i+3}")
+10.times do |i|
+  Buy.create(price: 100-(i+1), amount: 100, user_id: i, issue_id: 1)
+  Sell.create(price: 100+(i+1), amount: 100, user_id: i+10, issue_id: 1)
 end
