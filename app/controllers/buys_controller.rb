bb1821a29335ebb93ee100aee37fed286ce5c63f
syukan3 <en421430@s.okayama-u.ac.jp>
2018-12-22 21:06:45 +0900

    12/22-3

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -9,7 +9,7 @@ class BuysController < ApplicationController
   end
 
   def create
-    @issue = Issue.find_by(params[:issue_id])
+    @issue = Issue.find_by(id: params[:issue_id])
     @buy = Buy.new(price: params[:price], amount: params[:amount], user_id: current_user.id, issue_id: @issue.id)
     if @buy.save
       flash[:notice] ="注文しました。"
